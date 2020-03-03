6d9bfd2aeec419ac3301b44c31333ff5f7ee1d5b
张牡丹 <1050339760@qq.com>
2020-03-03 16:00:00 +0800

    分页器

--- admin/js/article_list.js
+++ admin/js/article_list.js
@@ -19,23 +19,25 @@ $(function () {
   // |  type   | string | 文章类型id，可以为空，为空返回所有类型文章   |
   // |  state  | string | 文章状态，草稿 ，已发布,为空返回所有状态文章 |
   // |  page   | number | 当前页，为空返回第1页 |
-  // | perpage | number | 每页显示条数，为空默认每页6条                
-  //点击筛选 form表单有默认提交的行为
-  $('#btnSearch').click(function (e) {
-    e.preventDefault();
-    let type = $('#selCategory').val();
-    let state = $('#selStatus').val();
-    let key = '';
-    let page = 1;
-    let perpage = 10;
-    //获取文章数据并渲染
+  // | perpage | number | 每页显示条数，为空默认每页6条 
+
+  //文章接口所需公共参数-初始化
+  let type = ''
+  let state = ''
+  let key = '';
+  let page = 1;
+  let perpage = 10;
+
+  //获取文章数据并渲染过程封装
+  function renList() {
     $.ajax({
       type: "get",
       url: urls.article_query,
       data: {
         type: type,
         state: state,
-        perpage: perpage
+        perpage: perpage,
+        page: page
       },
       dataType: "json",
       success: function (response) {
@@ -43,16 +45,58 @@ $(function () {
         if (response.code === 200) {
           const strArr = template('myList', response.data)
           $('tbody').html(strArr)
+          //获取总页数
+          let total = response.data.totalPage
+          renPagination(total, page)
         }
       }
     });
+  }
+  //点击筛选 form表单有默认提交的行为
+  $('#btnSearch').click(function (e) {
+    e.preventDefault();
+    //获取下拉框内容
+    type = $('#selCategory').val();
+    state = $('#selStatus').val();
+    //需要初始化页面数据
+    page = 1
+    renList()
   });
-  //主动触发
+
+
+  //进入页面主动触发
   $('#btnSearch').click()
-  //分页
-  // $('.pagination').on('click', 'li', function () {
-  //   $(this).addClass('active').siblings().removeClass('active')
-  // })
+
+
+  //封装分页器 totalPages 总页数 startPage 当前页数
+  function renPagination(totalPages, startPage) {
+    //动态分页 需要先销毁在创建新的
+    $('#pagination-demo').twbsPagination('destroy')
+    //创建新分页
+    $('#pagination-demo').twbsPagination({
+      //总页数
+      totalPages: totalPages,
+      //可见页数
+      visiblePages: 7,
+      //当前页
+      startPage: startPage,
+      first: ' 首页',
+      prev: '上一页',
+      next: '下一页',
+      last: '尾页',
+      // 点击事件回调,分页器会自动触发当前页
+      onPageClick: function (event, currentPage) {
+        // $('#page-content').text('Page ' + currentPage);
+        //把当前点击页赋值给公共变量中
+        page = currentPage;
+        // 根据当前页获取列表 点击页和当前页不一样时触发
+        if (currentPage != startPage) {
+          renList();
+        }
+
+      }
+    });
+  }
 
 
 })
